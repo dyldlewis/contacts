e73c1d0ea0a78df7806fa05d597d5cd7697fc54e
Max Scher and Rena Rallis <max@>
2017-06-12 09:11:41 -0700

    add functionality to project

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,40 @@
-function Contact(first, last) {
+//business logic
+function Contact(first, last, address, email) {
   this.firstName = first;
   this.lastName = last;
+  this.address = address;
+  this.email = email;
 }
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+// user interface logic
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedAddress = $("input#new-address").val();
+    var inputtedEmail = $("input#new-email").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedAddress, inputtedEmail);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    // $("input#new-first-name").val("");
+    // $("input#new-last-name").val("");
+
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $(".address").text(newContact.address);
+      $(".email").text(newContact.email);
+    });
+  });
+});
