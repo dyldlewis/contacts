d6674862a0edb276570d6775cf24939e407dbf21
Dylan Lewis and Max Scher <max@>
2017-06-13 09:32:03 -0700

    add fadeIn for displaying contact details

--- js/scripts.js
+++ js/scripts.js
@@ -62,7 +62,7 @@ $(document).ready(function() {
 
 
     $(".contact").last().click(function() {
-      $("#show-contact").show();
+      $("#show-contact").fadeIn();
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
